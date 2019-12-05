b97467c2271df3f878db1c6dec34e1b5ea4dab93
onel-shina <onelraad@gmail.com>
2019-12-05 00:39:46 -0800

    added second approach using provided shell

---
+++ src/main/java/com/onelshina/csis/LargestFile2.java
@@ -0,0 +1,91 @@
+package com.onelshina.csis;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Finds the largest file using DFS (Depth First Search).
+ */
+
+public class LargestFile2 {
+
+    /**
+     * If no start location is given, the we start the search in the current directory
+     *
+     * @param args {@link String}[] start location for the largest file search.
+     */
+    public static void main(String[] args) {
+        final Path path = Paths.get(args.length < 1 ? "." : args[0]);
+        final File file = findLargestFile(path);
+        if (file != null) {
+            System.out.printf("Starting at : %s, the largest file was found here:\n%s\n its size is: %d bytes\n",
+                    path.toAbsolutePath().toString(),
+                    file.getAbsolutePath(),
+                    file.length());
+        }
+    }
+
+    /**
+     * Identifies the more extreem of two given files.
+     * Modifying this method allows to search for other extreems, like smallest, oldest, etc.
+     *
+     * @param file1 {@link File} 1st file
+     * @param file2 {@link File} 2nd file
+     * @return {@link File} the more extreme of the two given files.
+     */
+
+    protected static File extreme(final File file1, final File file2){
+        File largestFile;
+        if(file1.toPath().getNameCount() > file2.toPath().getNameCount()){
+            largestFile = file1;
+        }
+        else if(file1.toPath().getNameCount() < file2.toPath().getNameCount()){
+            largestFile = file2;
+        }
+        else {
+            largestFile = file1;
+        }
+        return largestFile;
+    }
+    /**
+     * DFS for the most extreme file, starting the search at a given directory path.
+     *
+     * @param path {@link Path} path to a directory
+     * @return {@link File} most extreme file in the given path
+     */
+
+    protected static File findLargestFile(final Path path) {
+        return largestFile(path.toFile(), null, 0);
+    }
+
+
+    private static File largestFile(final File inputFile, final File largestFileFound, final long largestFileFoundSize) {
+        File largestFile = largestFileFound;
+        long largestFileSize = largestFileFoundSize;
+        File comparisonFile = null;
+        File[] files = inputFile.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    comparisonFile = file;
+                } else if (file.isDirectory()) {
+                    comparisonFile = largestFile(file, largestFile, largestFileSize);
+                }
+                if (comparisonFile != null) {
+                    long comparisonFileLength = comparisonFile.length();
+                    if (comparisonFileLength > largestFileSize) {
+                        largestFile = comparisonFile;
+                        largestFileSize = comparisonFileLength;
+                    } else if (comparisonFileLength == largestFileSize) {
+                        largestFile = extreme(largestFile, comparisonFile);
+                    }
+                }
+            }
+        }
+        if (largestFile == null){
+            System.out.printf("No Files were found in: %s", inputFile.toPath().toAbsolutePath());
+        }
+        return largestFile;
+    }
+}
---
+++ src/test/java/com/onelshina/csis/LargestFile2Test.java
@@ -0,0 +1,104 @@
+package com.onelshina.csis;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class LargestFile2Test {
+
+    /* To create temporary directories and files for testing
+     * that will be deleted if test fails or if it runs
+     * successfully
+     */
+    @Rule
+    public TemporaryFolder temporary = new TemporaryFolder();
+
+
+    @Test
+    public void extreme() throws IOException {
+        File directory1 = temporary.newFolder("TestDirectory");
+        //Create another directory within directory1
+        File directory2 = temporary.newFolder(directory1.getName(), "TestDirectory2");
+
+        File file1 = temporary.newFile("TestDirectory/test.txt");
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file1, "rw");
+        randomAccessFile.setLength(5); //Changing the size of the file
+        randomAccessFile.close();
+
+        File file2 = temporary.newFile("TestDirectory/TestDirectory2/test2.txt");
+        randomAccessFile = new RandomAccessFile(file2, "rw");
+        randomAccessFile.setLength(5); //Changing the size of the file
+        randomAccessFile.close();
+
+        File file3 = temporary.newFile("TestDirectory/TestDirectory2/test3.txt");
+        randomAccessFile = new RandomAccessFile(file3, "rw");
+        randomAccessFile.setLength(5); //Changing the size of the file
+        randomAccessFile.close();
+
+        //Both same size, file2 has longer path
+        Assert.assertEquals(LargestFile2.extreme(file1, file2), file2);
+        /*Both same size and path length, but first found file get passed into extreme method first
+        * Therefore, we expect the extreme method to return the first file that was found
+        * Which would be file2 in this case
+         */
+        Assert.assertEquals(LargestFile2.extreme(file2, file3), file2);
+
+    }
+
+    /**
+     * BFS (Breadth First Search) to find all the files within the
+     * given directory and its subdirectories
+     *
+     * @param path {@link Path} the file representing the directory we want to search
+     * @return {@link File} largest file
+     * and it's subdirectories
+     */
+
+    @SuppressWarnings({"SameParameterValue", "unchecked"})
+    private static File findLargestFileTest(final Path path) {
+        File largestFile = null;
+        long largestFileSize = 0;
+        File dir = path.toFile();
+        final ArrayList<File> fileAndDirectories = new ArrayList<File>();
+        fileAndDirectories.add(dir);
+
+        while (!fileAndDirectories.isEmpty()) {
+            final File f = fileAndDirectories.remove(0);
+            if (f.isFile()) {
+                long fileSize = f.length();
+                if (fileSize > largestFileSize) {
+                    largestFile = f;
+                    largestFileSize = fileSize;
+                }
+                else if (fileSize == largestFileSize) {
+                    largestFile = LargestFile2.extreme(largestFile, f);
+                }
+            } else if (f.isDirectory()) {
+                final File[] fa = f.listFiles();
+                if (fa != null) {
+                    fileAndDirectories.addAll(Arrays.asList(fa));
+                }
+            }
+        }
+        return largestFile;
+    }
+
+    @Test
+    public void findLargestFile() {
+        Path currentRelativePath = Paths.get("").toAbsolutePath().getParent();
+        File fileMain = LargestFile2.findLargestFile(currentRelativePath);
+        File fileTest = LargestFile2Test.findLargestFileTest(currentRelativePath);
+        Assert.assertEquals(fileMain, fileTest);
+    }
+}
